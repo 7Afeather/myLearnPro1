9f925d0a8388aff222eed7a7c5fc3018617e8f7e
杨云杰 <1924319618@qq.com>
2024-05-13 10:18:03 +0800

    fix:添加第一个vue项目

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # myLearnPro1
+
+git branch -M master 创建本地master分支/修改当前分支名称为master
+git remote add origin git@github.com:7Afeather/myLearnPro1.git 将本地仓库和远程仓库建立联系
+git push -u origin master push到远程分支/第一次加了-u，后面就可以直接git push推送
---
+++ index.js
@@ -0,0 +1,7 @@
+console.log(
+  JSON.parse(
+    '[{"fillItem":"test3","fillItemId":219518591267921920,"fillItemResult":"10"},{"fillItem":"test3","fillItemId":219521431864500224,"fillItemResult":"10"}]'
+  )
+);
+console.log(JSON.parse("219521431864500224"));
+console.log(new Array(13).fill(0));
