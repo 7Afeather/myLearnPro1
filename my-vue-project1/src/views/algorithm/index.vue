13d44d4781b82f5d55352b5cd6969677e8de60ff
杨云杰 <1924319618@qq.com>
2024-05-20 18:26:12 +0800

    fix:看不懂动态规划

--- my-vue-project1/src/views/algorithm/index.vue
+++ my-vue-project1/src/views/algorithm/index.vue
@@ -20,27 +20,67 @@
 //         )
 // return dp[N][W]
 
-const knapsack = (W: number, N: number, wt: number[], val: number[]): number => {
-  //   if (N != wt.length) return 0;
-  const dp: number[][] = [];
-  // int[][] dp = new int[N + 1][W + 1];
-  // 能装N个物品
-  for (let i = 1; i <= N; i++) {
-    // 能装重量为W
-    for (let w = 1; w <= W; w++) {
-      // wt[i-1]是第i-1个物品的重量
-      if (w - wt[i - 1] < 0) {
-        // 如果单个物品的重量就已经大于背包容纳的重量了，只能不装
-        dp[i][w] = dp[i - 1][w];
+// const knapsack = (W: number, N: number, wt: number[], val: number[]): number => {
+//   //   if (N != wt.length) return 0;
+//   const dp: number[][] = [];
+//   // int[][] dp = new int[N + 1][W + 1];
+//   // 能装N个物品
+//   for (let i = 1; i <= N; i++) {
+//     // 能装重量为W
+//     for (let w = 1; w <= W; w++) {
+//       // wt[i-1]是第i-1个物品的重量
+//       if (w - wt[i - 1] < 0) {
+//         // 如果单个物品的重量就已经大于背包容纳的重量了，只能不装
+//         dp[i][w] = dp[i - 1][w];
+//       } else {
+//         // dp[i - 1][w - wt[i - 1]]
+//         dp[i][w] = Math.max(dp[i - 1][w - wt[i - 1]] + val[i - 1], dp[i - 1][w]);
+//       }
+//     }
+//   }
+
+//   return dp[N][W];
+// };
+
+// console.log(knapsack(3, 2, [1, 2, 3], [3, 2, 1]));
+
+function knapsack(weights: number[], values: number[], w: number) {
+  const n = weights.length - 1; // 获取盒子个数i  从盒子0开始
+  const f: number[][] = [[]]; //定义f的矩阵
+  for (let j = 0; j <= w; j++) {
+    // 背包的重量j从0开始，一直到我们输入的w
+    if (j < weights[0]) {
+      // 背包的重量小于盒子0的重量，价值为0
+      f[0][j] = 0;
+    } else {
+      f[0][j] = values[0]; // 否则容量为物品0的价值
+    }
+  }
+  console.log('f1', f);
+  // 上述for循环实现了方程式的前两条
+  for (let j = 0; j <= w; j++) {
+    // 当盒子数不是1个时
+    for (let i = 1; i <= n; i++) {
+      if (!f[i]) {
+        // 创建新的一行
+        f[i] = [];
+      }
+      if (j < weights[i]) {
+        // 等于之前的最优值
+        f[i][j] = f[i - 1][j];
       } else {
-        // dp[i - 1][w - wt[i - 1]]
-        dp[i][w] = Math.max(dp[i - 1][w - wt[i - 1]] + val[i - 1], dp[i - 1][w]);
+        f[i][j] = Math.max(f[i - 1][j], f[i - 1][j - weights[i]] + values[i]);
+        console.log(
+          'f[i - 1][j],f[i - 1][j - weights[i]] + values[i]',
+          f[i - 1][j],
+          f[i - 1][j - weights[i]] + values[i],
+        );
       }
     }
   }
-
-  return dp[N][W];
-};
-
-console.log(knapsack(3, 2, [1, 2, 3], [3, 2, 1]));
+  console.log('f2', f);
+  //   return f[n][w];
+}
+const a = knapsack([2, 2, 6, 5, 4], [6, 3, 5, 4, 6], 10);
+console.log(a);
 </script>
